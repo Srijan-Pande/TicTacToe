956d6ad90835f23c4f561807adc0b4b70a497b01
Srijan Pande <srijanpandecodes@gmail.com>
2026-02-01 16:28:06 +0530

    Wrote a function to destroy a board

--- Functions.c
+++ Functions.c
@@ -5,12 +5,20 @@
 #include<stdbool.h>
 #include<errno.h>
 
-char *create_board(void)
+char *create_board_internal(void)
 {
     char *board = malloc(9*sizeof(char));
     if(board==nullptr)return nullptr;
     memset(board,' ',9);
     return board;
+}
+
+void destroy_board_internal(char **board)
+{
+    if(!board||!*board)return;
+    free(*board);
+    *board=nullptr;
+    return;
 }
 
 int main(int argc, char *argv[])
